2472793a6353593e57c5d1571c4ca34ce9fd848e
Ramiro Uffelmann <ramirouf@gmail.com>
2022-09-02 16:14:59 -0300

    Finished 6th class (GET practice)

--- index.js
+++ index.js
@@ -11,11 +11,37 @@ app.get('/new-route', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
-  res.json({
-    name: "Product 1",
-    price: 999
-  });
+  res.json([
+    {
+      name: "Product 1",
+      price: 1000
+    },
+    {
+      name: "Product 2",
+      price: 2000
+    }
+  ]);
 });
+
+app.get("/products/:id", (req, res) => {
+  //const id = req.params.id;
+  const { id } = req.params;
+  res.json({
+    id,
+    name: "Product 2",
+    price: 2000
+  });
+})
+
+app.get("/categories/:catID/products/:prodID", (req, res) => {
+  const { catID, prodID } = req.params;
+  res.json({
+    catID,
+    prodID,
+    /*name: "Product !",
+    price: 2000,*/
+  });
+})
 
 app.listen(port, () => {
   console.log("Listening on port " + port);

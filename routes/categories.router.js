9638a56fda5759215c1f7fd88c5b415061b19b89
Ramiro Uffelmann <ramirouf@gmail.com>
2022-09-06 14:21:45 -0300

    Separeted categories functionalities to a service

--- routes/categories.router.js
+++ routes/categories.router.js
@@ -1,14 +1,35 @@
 const express = require("express");
-const router = express.Router();
+const CategoriesService = require("../services/categories.service")
 
-router.get("/categories/:catID/products/:prodID", (req, res) => {
-  const { catID, prodID } = req.params;
-  res.json({
-    catID,
-    prodID,
-    /*name: "Product !",
-    price: 2000,*/
-  });
-})
+const router = express.Router();
+const service = new CategoriesService();
+
+router.get('/', (req, res) => {
+  const categories = service.find();
+  res.json(categories);
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const category = service.findOne(id);
+  if (category) {
+    res.status(200).json({
+      message: "Category found !",
+      category
+    });
+  } else {
+    res.status(404).json({
+      message: "Error. Category not found",
+    })
+  }
+});
+
+router.post("/", (req, res) => {
+  const body = req.body;
+  res.status(201).json({
+    message: "Category created !",
+    data: body
+  })
+});
 
 module.exports = router;

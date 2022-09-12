76b63d38c4a8682783c6f06c1702df6ab211c2fb
Ramiro Uffelmann <ramirouf@gmail.com>
2022-09-12 14:43:07 -0300

    Finished 18th class (Error handling with Bomm)

--- services/product.service.js
+++ services/product.service.js
@@ -1,5 +1,5 @@
 const faker = require("faker");
-
+const boom = require("@hapi/boom")
 
 class ProductsService {
 
@@ -16,6 +16,7 @@ class ProductsService {
         name: faker.commerce.productName(),
         price: parseInt(faker.commerce.price(), 10),
         image: faker.image.imageUrl(),
+        isBlocked: faker.datatype.boolean(),
       });
     }
   }
@@ -39,14 +40,23 @@ class ProductsService {
   }
 
   async findOne(id) {
-    const name = this.getTotal();
-    return this.products.find(item => item.id === id);
+    //const name = this.getTotal();
+    //return this.products.find(item => item.id === id);
+    const product = this.products.find(item => item.id === id);
+    if (!product) {
+      throw boom.notFound("Product not found");
+    }
+    if (product.isBlocked) {
+      throw boom.conflict("Product is blocked");
+    }
+    return product;
   }
 
   async update(id, changes) {
     const index = this.products.findIndex(item => item.id === id);
     if (index === -1) {
-      throw new Error("Product not found !");
+      //throw new Error("Product not found !");
+      throw boom.notFound("Product not found");
     }
     const product = this.products[index];
     this.products[index] = {
@@ -60,7 +70,8 @@ class ProductsService {
   async delete(id) {
     const index = this.products.findIndex(item => item.id === id);
     if (index === -1) {
-      throw new Error("Product not found !");
+      //throw new Error("Product not found !");
+      throw boom.notFound("Product not found");
     }
     this.products.splice(index, 1);
     return { id, message: "Eliminated" }

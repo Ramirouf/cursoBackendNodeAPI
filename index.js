7521500bac06dde22f630cde763a4c8909afa008
Ramiro Uffelmann <ramirouf@gmail.com>
2022-09-29 14:04:00 -0300

    Added deployment config

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 //const { application } = require('express');
 const express = require('express');
+const cors = require("cors")
 const routerApi = require("./routes");
 
 const { logErrors, errorHandler, boomErrorHandler } = require("./middlewares/error.handler");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
+const whitelist = ["http://localhost:8080", "https://myapp.com"];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed"));
+    }
+  }
+}
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send("Hello from express server");
